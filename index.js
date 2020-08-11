f3a228771f6096e1669a270de586f8d79b7896b2
Ayodejioladimeji <layobright11@gmail.com>
2020-08-11 16:11:08 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// THE SECTION OF THE DATE
+const dateElement = document.getElementById("date");
+const options = {
+    weekday:"long",
+    month:"short",
+    day:"numeric"
+};
+
+const today  = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+
+function update(){
+    const amount = Number(document.getElementById("input").value);
+    const percent = document.getElementById("range").value;
+
+    let percentage = (amount/100)*percent;
+
+
+    document.getElementById("output").innerHTML = percentage;
+    document.getElementById("percent").innerHTML = percent + "%";
+
+}
+
+const container = document.getElementById("container");
+container.addEventListener("input", update);
